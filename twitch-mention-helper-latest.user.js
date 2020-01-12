04a05597e7c5f13fd02f6c5f06e0aaf26cedf77a
Tom O'Neill <TomONeill@users.noreply.github.com>
2020-01-12 17:18:14 +0100

    Added mention settings icon

--- twitch-mention-helper-latest.user.js
+++ twitch-mention-helper-latest.user.js
@@ -18,13 +18,18 @@
 /*
 TODO:
 - Button that lights up after receiving new mentions
-- View mentions somewhere inline
+- Clicking on the button views mentions inline + settings
+- Settings include:
+  Turning sound notification on/off
+  Adding other keywords to get notified on
+  Turn headsup notification on/off
 Later:
 - Include source (channel name) of mention
 - Save chat emotes (img alt)
 - Support a set of notification sounds
 - Custom notification sound (per username)
 - Keep mention history option? (localStorage)
+- Heads up notification
 */
 
 const _DEBUG = true;
@@ -46,6 +51,7 @@ function initialise() {
 	setupStateChangeEvent();
 	initialiseUsernamesToTrack();
 	initialiseStateChangeEvent();
+	addHtml();
 	setTimeout(() => tryAttachToChat());
 }
 
@@ -250,5 +256,26 @@ function playNotificationSound() {
 	const audio = new Audio("data:audio/wav;base64," + base64Audio);
 	audio.play();
 }
+
+function addHtml() {
+	addMentionIcon();
+}
+
+function addMentionIcon() {
+	const mentionIconNode = createNode(ICON_AT);
+	const chatInputButtonsContainer = document.getElementsByClassName("chat-input__buttons-container")[0];
+	const chatInputButtons = chatInputButtonsContainer.children[1];
+	const chatSettingsButton = chatInputButtons.children[1];
+	chatSettingsButton.parentNode.insertBefore(mentionIconNode, chatSettingsButton);
+}
+
+function createNode(html) {
+	var wrapper= document.createElement("div");
+	wrapper.innerHTML= html;
+	return wrapper.firstChild;
+}
+
+const ICON_AT = `<div class="tw-relative"><div><div class="tw-inline-flex tw-relative tw-tooltip-wrapper"><div class="tw-z-default"><button class="tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-core-button tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative" data-a-target="chat-settings" aria-label="Mentions"><span class="tw-button-icon__icon"><div style="width: 2rem; height: 2rem;"><div class="tw-align-items-center tw-full-width tw-icon tw-icon--fill tw-inline-flex"><div class="tw-aspect tw-aspect--align-top"><div class="tw-aspect__spacer" style="padding-bottom: 100%;"></div><svg class="tw-icon__svg" width="100%" height="100%" version="1.1" viewBox="0 0 20 20" x="0px" y="0px"><g><path d="M10 8a2 2 0 100 4 2 2 0 000-4z"></path><path fill-rule="evenodd" d="M9 2h2a2.01 2.01 0 001.235 1.855l.53.22a2.01 2.01 0 002.185-.439l1.414 1.414a2.01 2.01 0 00-.439 2.185l.22.53A2.01 2.01 0 0018 9v2a2.01 2.01 0 00-1.855 1.235l-.22.53a2.01 2.01 0 00.44 2.185l-1.415 1.414a2.01 2.01 0 00-2.184-.439l-.531.22A2.01 2.01 0 0011 18H9a2.01 2.01 0 00-1.235-1.854l-.53-.22a2.009 2.009 0 00-2.185.438L3.636 14.95a2.009 2.009 0 00.438-2.184l-.22-.531A2.01 2.01 0 002 11V9c.809 0 1.545-.487 1.854-1.235l.22-.53a2.009 2.009 0 00-.438-2.185L5.05 3.636a2.01 2.01 0 002.185.438l.53-.22A2.01 2.01 0 009 2zm-4 8l1.464 3.536L10 15l3.535-1.464L15 10l-1.465-3.536L10 5 6.464 6.464 5 10z" clip-rule="evenodd"></path></g></svg></div></div></div></span></button></div><div class="tw-tooltip tw-tooltip--align-center tw-tooltip--left" data-a-target="tw-tooltip-label" role="tooltip" id="773db2352383a2ec8baf5a20407cce78">Mentions</div></div></div></div>`;
+const SETTINGS = `<div class="tw-absolute tw-balloon tw-balloon--auto tw-balloon--right tw-balloon--up tw-block" data-a-target="chat-settings-balloon" role="dialog" style="margin-right: -5.3rem;"><div class="tw-border-radius-large tw-c-background-base tw-c-text-inherit tw-elevation-2"><div class="chat-settings__popover"><div class="chat-settings__header tw-align-items-center tw-c-background-base tw-flex tw-pd-x-1 tw-relative"><div class="chat-settings__back-icon-container tw-left-0 tw-mg-r-05"></div><div class="tw-align-center tw-align-items-center tw-flex tw-flex-grow-1 tw-justify-content-center"><p class="tw-c-text-alt tw-font-size-5 tw-semibold">Chat mentions</p></div><div class="tw-mg-l-05 tw-right-0"><button class="tw-align-items-center tw-align-middle tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-border-top-left-radius-medium tw-border-top-right-radius-medium tw-button-icon tw-core-button tw-inline-flex tw-interactive tw-justify-content-center tw-overflow-hidden tw-relative" data-test-selector="chat-settings-close-button-selector" aria-label="Close"><span class="tw-button-icon__icon"><div style="width: 2rem; height: 2rem;"><div class="tw-align-items-center tw-full-width tw-icon tw-icon--fill tw-inline-flex"><div class="tw-aspect tw-aspect--align-top"><div class="tw-aspect__spacer" style="padding-bottom: 100%;"></div><svg class="tw-icon__svg" width="100%" height="100%" version="1.1" viewBox="0 0 20 20" x="0px" y="0px"><g><path d="M8.5 10L4 5.5 5.5 4 10 8.5 14.5 4 16 5.5 11.5 10l4.5 4.5-1.5 1.5-4.5-4.5L5.5 16 4 14.5 8.5 10z"></path></g></svg></div></div></div></span></button></div></div><div class="chat-settings scrollable-area scrollable-area--suppress-scroll-x" data-test-selector="scrollable-area-wrapper" data-simplebar="init"><div class="simplebar-track vertical" style="visibility: hidden;"><div class="simplebar-scrollbar"></div></div><div class="simplebar-track horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar"></div></div><div class="simplebar-scroll-content" style="padding-right: 14px; margin-bottom: -28px;"><div class="simplebar-content" style="padding-bottom: 14px; margin-right: -14px;"><div><div class="chat-settings__content tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-c-background-base tw-c-text-base tw-pd-1"><div class="tw-c-background-base tw-c-text-base tw-flex-column tw-full-width tw-inline-flex"><button class="tw-block tw-border-radius-medium tw-full-width tw-interactable tw-interactable--hover-enabled tw-interactable--inverted tw-interactive" data-a-target="edit-appearance-button"><div class="name-display tw-align-items-center tw-flex tw-justify-content-between tw-pd-05"><div class="tw-align-items-center tw-flex"><span class="tw-strong"><span data-a-target="edit-display-name" class="name-display__name">TomONeill</span></span></div></div></button></div><div class="tw-border-t tw-mg-t-1 tw-mg-x-05 tw-pd-b-1"></div><div class=""><div class="tw-mg-y-05 tw-pd-x-05"><p class="tw-c-text-alt-2 tw-font-size-6 tw-strong tw-upcase">My Preferences</p></div><div class="tw-pd-05"><div data-a-target="timestamp-checkbox" data-test-selector="timestamps-selector" class="tw-align-items-center tw-flex"><label class="tw-drop-down-menu-input-item__label tw-flex-grow-1" for="chat-settings-timestamp">Show Headsup Notifications</label><div class="tw-toggle" data-a-target="timestamp-checkbox" data-test-selector="timestamps-selector"><input type="checkbox" id="chat-settings-timestamp" label="Show Timestamp" class="tw-toggle__input" data-a-target="tw-toggle"><label for="chat-settings-timestamp" class="tw-toggle__button"><p class="tw-hide-accessible">Show Timestamp</p></label></div></div></div><div class="tw-pd-05"><div data-a-target="high-contrast-color-checkbox" data-test-selector="high-contrast-selector" class="tw-align-items-center tw-flex"><label class="tw-drop-down-menu-input-item__label tw-flex-grow-1" for="chat-settings-high-contrast">Play Notification Sounds</label><div class="tw-toggle" data-a-target="high-contrast-color-checkbox" data-test-selector="high-contrast-selector"><input type="checkbox" id="chat-settings-high-contrast" label="Readable Colors" class="tw-toggle__input" data-a-target="tw-toggle" checked=""><label for="chat-settings-high-contrast" class="tw-toggle__button"><p class="tw-hide-accessible">Readable Colors</p></label></div></div></div><div class="tw-full-width tw-relative"><button class="tw-block tw-border-radius-medium tw-full-width tw-interactable tw-interactable--alpha tw-interactable--hover-enabled tw-interactive" data-a-target="popout-chat-button" data-test-selector="popout-button"><div class="tw-align-items-center tw-flex tw-pd-05 tw-relative"><div class="tw-flex-grow-1">Add Notification Keywords</div></div></button></div></div></div></div></div></div></div></div></div></div>`;
 
 if (_DEBUG) { console.info("Script loaded."); }
